3cb21db31376ef653f45928907bc450f309a2d7b
Karnagie <vova333dec@gmail.com>
2020-01-26 22:12:40 +0600

    Added comments

--- Program.cs
+++ Program.cs
@@ -10,31 +10,57 @@ namespace sorts
             System.Diagnostics.Stopwatch sw = new Stopwatch();
             sw.Start();
 
-            int[] arr  = new int[100000];
+            int[] arr    = new int[100000];
             int[] sorted = new int[arr.Length];
             Random ran = new Random();
 
-            //Console.Write("Not sorted array: ");
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = ran.Next(0, 10000);
-                //Console.Write(arr[i]+" ");
             }
-            //Console.WriteLine();
-            //Console.Write("Sorted          : ");
+
+            Sort(arr, ref sorted);
+            //Debug(arr, ref sorted);
+        }
+
+        private static void Debug(int[] arr, ref int[] sorted){
+            System.Diagnostics.Stopwatch sw = new Stopwatch();
+
+            Console.Write("Not sorted array: ");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i]+" ");
+
+            Console.WriteLine();
+            Console.Write("Sorted          : ");
             
             sw.Start();
-            sorted = Sorts.RadixSort(arr);
-            //sorted = Sorts.BubbleSort(arr);
+            SortMode(arr, ref sorted);
             sw.Stop();
 
             for (int i = 0; i < sorted.Length; i++)
             {
-                //Console.Write(sorted[i]+" ");
+                Console.Write(sorted[i]+" ");
             }
-            //Console.WriteLine();
+            Console.WriteLine();
             sw.Stop();
             Console.WriteLine("Done:"+(sw.ElapsedMilliseconds/100.0).ToString());
+        }
+
+        private static void Sort(int[] arr, ref int[] sorted){
+            System.Diagnostics.Stopwatch sw = new Stopwatch();
+            
+            sw.Start();
+            SortMode(arr, ref sorted);
+            sw.Stop();
+
+            Console.WriteLine();
+            sw.Stop();
+            Console.WriteLine("Done:"+(sw.ElapsedMilliseconds/100.0).ToString());
+        }
+
+        private static void SortMode(int[] arr, ref int[] sorted){
+            sorted = Sorts.RadixSort(arr);
+            //sorted = Sorts.BubbleSort(arr);
         }
     }
 }
--- Sorts.cs
+++ Sorts.cs
@@ -4,20 +4,26 @@ public static class Sorts{
 
     #region RadixSort
     public static int[] RadixSort(int[] input){
-        int mx = 0;
-        int e = 0;
+
+        int mx = 0; // Biggest number
+        int e  = 0; // Amount of right shift
+
         int[] output = new int[input.Length];
+
+        // Copying numbers from the input to the output
         for (int i = 0; i < output.Length; i++)
             output[i] = input[i];
         
-
+        // Finding biggest number from the input
         mx = input[0];
         for (int i = 1; i < input.Length; i++)
             if(mx < input[i]) mx = input[i];
 
         while (mx > 0)
         {
+            // Sorting the output and shift by e bits
             output = Radix(output, e);
+            // shift the biggest number by 8
             mx = mx >> 8;
             e+=8;
         }
@@ -27,19 +33,22 @@ public static class Sorts{
 
     private static int[] Radix(int[] input, int e){
         int[] output = new int[input.Length];
-        int[] dist = new int[0x100];
-        int[] ind = new int[0x100];
+        int[] dist   = new int[0x100];
+        int[] ind    = new int[0x100];
 
+        // Fill tha array dist by numbers from input
         for (int i = 0; i < input.Length; i++)
             dist[(input[i] >> e) & 0xFF ] = dist[(input[i] >> e) & 0xFF]+1;
 
         ind[0] = 0;
 
+        // Set indexes by numbers from the dist
         for (int i = 1; i < 0x100; i++)
         {
             ind[i] = ind[i-1]+dist[i-1];
         }
 
+        // Fill the output
         for (int i = 0; i < input.Length; i++){
             output[ ind[(input[i] >> e) & 0xFF] ] = input[i];
             ind[(input[i] >> e) & 0xFF] = ind[(input[i] >> e) & 0xFF]+1;
@@ -51,7 +60,7 @@ public static class Sorts{
 
     #region BubbleSort
     public static int[] BubbleSort(int[] input){
-        int n = 0;
+        int n = 0; // Number for swapping
         int[] output = new int[input.Length];
         for (int i = 0; i < output.Length; i++)
             output[i] = input[i];
@@ -61,7 +70,7 @@ public static class Sorts{
             for (int j = 0; j < output.Length-i-1; j++)
             {
                 if(output[j] > output[j+1]){
-                    n = output[j];
+                    n           = output[j];
                     output[j]   = output[j+1];
                     output[j+1] = n;
                 }
